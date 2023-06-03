a0db4be5d314a6d68551ff176ceb247fcdcae07e
Leonardoguimaraesdev <leonardo.guimaraes.dev@gmail.com>
2023-06-03 14:58:00 -0300

    Adjust in the switch banner button

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -9,7 +9,7 @@ export default function Banners() {
     const [arrayBanner, setArrayBanner] = useState(['/DogBanner.jpg', '/DogBanner2.jpg', '/DogBanner3.jpg'])
 
     const changeBannerLeft = () => {
-        if(numArray === 0) {
+        if (numArray === 0) {
             return
         } else {
             setNumArray(numArray - 1)
@@ -17,7 +17,7 @@ export default function Banners() {
     }
 
     const changeBannerRight = () => {
-        if(numArray === 2) {
+        if (numArray === 2) {
             return
         } else {
             setNumArray(numArray + 1)
@@ -27,17 +27,22 @@ export default function Banners() {
     return (
         <section className={styles.bannersContainer}>
             <div className={styles.left}>
-                <svg onClick={(e) => changeBannerLeft()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className={styles.switchLeft}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
-                </svg>
+                {numArray !== 0 &&
+                    <svg onClick={(e) => changeBannerLeft()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className={styles.switchLeft}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
+                    </svg>
+                }
+
             </div>
             <div className={styles.banners}>
-                <Image className={styles.img} src={arrayBanner[numArray]} alt='Banner de filme' width={1170} height={637} priority/>
+                <Image className={styles.img} src={arrayBanner[numArray]} alt='Banner de filme' width={1170} height={637} priority />
             </div>
             <div className={styles.right}>
-                <svg onClick={(e) => changeBannerRight()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className={styles.switchRight}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
-                </svg>
+                {numArray !== 2 &&
+                    <svg onClick={(e) => changeBannerRight()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className={styles.switchRight}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+                    </svg>
+                }
             </div>
         </section>
     )
